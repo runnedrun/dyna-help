e45be619941834242c3e69d473c391c1b7d4ff33
David Gaynor <runnedrun@gmail.com>
2020-09-15 14:57:12 +0900

    this boilerplate ssetup is better

--- src/getDynalistSearchInfo.js
+++
@@ -1,4 +0,0 @@
-const dep = require("./dependency")
-
-console.log("running content script")
-dep()
--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,93 @@
-const { resolve } = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WebpackChromeReloaderPlugin = require("webpack-chrome-extension-reloader");
+var webpack = require("webpack"),
+    path = require("path"),
+    fileSystem = require("fs"),
+    env = require("./utils/env"),
+    CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin,
+    CopyWebpackPlugin = require("copy-webpack-plugin"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    WriteFilePlugin = require("write-file-webpack-plugin");
 
-const mode = process.env.NODE_ENV;
-module.exports = {
-  mode,
-  devtool: "inline-source-map",
+// load the secrets
+var alias = {};
+
+var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
+
+var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
+
+if (fileSystem.existsSync(secretsPath)) {
+  alias["secrets"] = secretsPath;
+}
+
+var options = {
+  mode: process.env.NODE_ENV || "development",
   entry: {
-    "content-script": "./src/getDynalistSearchInfo.js",
-    background: "./src/background.js",
-
-    // This is just the popup script, it shouldn't trigger the plugin reload when is changed
-    popup: "./src/popup.js"
+    popup: path.join(__dirname, "src", "js", "popup.js"),
+    options: path.join(__dirname, "src", "js", "options.js"),
+    background: path.join(__dirname, "src", "js", "background.js")
   },
   output: {
-    publicPath: ".",
-    path: resolve(__dirname, "dist/"),
-    filename: "[name].js",
-    libraryTarget: "umd"
+    path: path.join(__dirname, "build"),
+    filename: "[name].bundle.js"
   },
-  mode: "development",
-  plugins: [
-    /***********************************************************************/
-    /* By default the plugin will work only when NODE_ENV is "development" */
-    /***********************************************************************/
-    new WebpackChromeReloaderPlugin(),
-
-    new MiniCssExtractPlugin({ filename: "style.css" }),
-    new CopyWebpackPlugin({patterns: [
-      { from: "./src/popup.html"},
-      { from: "./manifest.json" },
-    ]})
-  ],
   module: {
     rules: [
       {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [require("@babel/preset-env")]
-          }
-        }
-      },
-      {
         test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader
-          },
-          "css-loader"
-        ]
+        loader: "style-loader!css-loader",
+        exclude: /node_modules/
       },
       {
-        test: /\.txt$/,
-        use: "raw-loader"
+        test: new RegExp('.(' + fileExtensions.join('|') + ')$'),
+        loader: "file-loader?name=[name].[ext]",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        exclude: /node_modules/
       }
     ]
-  }
+  },
+  resolve: {
+    alias: alias
+  },
+  plugins: [
+    // clean the build folder
+    new CleanWebpackPlugin(),
+    // expose and write the allowed env vars on the compiled bundle
+    new webpack.EnvironmentPlugin(["NODE_ENV"]),
+    new CopyWebpackPlugin([{
+      from: "src/manifest.json",
+      transform: function (content, path) {
+        // generates the manifest file using the package.json informations
+        return Buffer.from(JSON.stringify({
+          description: process.env.npm_package_description,
+          version: process.env.npm_package_version,
+          ...JSON.parse(content.toString())
+        }))
+      }
+    }]),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "popup.html"),
+      filename: "popup.html",
+      chunks: ["popup"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "options.html"),
+      filename: "options.html",
+      chunks: ["options"]
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "background.html"),
+      filename: "background.html",
+      chunks: ["background"]
+    }),
+    new WriteFilePlugin()
+  ]
 };
+
+if (env.NODE_ENV === "development") {
+  options.devtool = "cheap-module-eval-source-map";
+}
+
+module.exports = options;
